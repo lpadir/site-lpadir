9eba6339bc7227479eb49a6d1426ebd03dc28c4d
iurymelo <iurymelo@live.se>
2018-11-05 18:06:04 +0000

    1st Deploy to Production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     def new
         @article = Article.new
     end
@@ -16,15 +17,14 @@ class ArticlesController < ApplicationController
     end
     
     def show
-        @article = Article.find(params[:id])
+        
     end
     
     def edit
-        @article = Article.find(params[:id])
+        
     end
     
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:success] = "Artigo atualizado com sucesso!"
             redirect_to article_path(@article)
@@ -38,7 +38,6 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         
         flash[:danger] = "Artigo deletado com sucesso!"
@@ -47,6 +46,11 @@ class ArticlesController < ApplicationController
     end
     
     private
+    
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
     def article_params
         params.require(:article).permit(:titulo, :descricao)
     end
